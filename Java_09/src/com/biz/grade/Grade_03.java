3c12d393fa457add12cab1015333f453b4660560
unknown <maplegjswns@naver.com>
2020-05-28 17:46:38 +0900

    cars

---
+++ Java_09/src/com/biz/grade/Grade_03.java
@@ -0,0 +1,31 @@
+package com.biz.grade;
+
+import java.util.Random;
+
+public class Grade_03 {
+	
+	public static void main(String[] args) {
+		
+		int Num1 = 0;
+		
+		
+		Random rnd = new Random();
+		
+		int Num2 = Num1 * 55;
+				
+		
+		
+		
+				
+		
+		
+		
+		System.out.println(Num2);
+		
+		
+		
+		
+		
+	}
+
+}
